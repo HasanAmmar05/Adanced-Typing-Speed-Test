7b27e480065ab9f5fe1032517ae8f6d2a027ac7e
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-29 17:08:21 +0800

    Light/Dark mode toggle theme component

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SunIcon, MoonIcon } from '@heroicons/react/solid'
+
+function ThemeToggle({ isDarkMode, setIsDarkMode }) {
+  return (
+    <button
+      onClick={() => setIsDarkMode(!isDarkMode)}
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-600"
+    >
+      {isDarkMode ? (
+        <SunIcon className="h-6 w-6 text-yellow-400" />
+      ) : (
+        <MoonIcon className="h-6 w-6 text-gray-800" />
+      )}
+    </button>
+  )
+}
+
+export default ThemeToggle
